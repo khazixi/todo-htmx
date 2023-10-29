6dce763fd31a3ed02760224ab3f6b150aa71a5fd
Khazixi <khazixfangirl@gmail.com>
2023-10-29 17:14:55 -0400

    TodoMVC finished, a lot of work skipped

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { Hono } from 'hono'
 import { validator } from 'hono/validator'
 import { todo } from './schema'
 import { db } from './db'
+import { desc, eq } from 'drizzle-orm'
 
 const app = new Hono()
 
@@ -16,9 +17,12 @@ const HTML: FC = (props) => {
         <title> TODO MVC with Hono </title>
         {
           /*
-        <script src="https://unpkg.com/htmx.org@1.9.6" integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni" crossorigin="anonymous"></script>
            */
         }
+        <script src="https://unpkg.com/htmx.org@latest"></script>
+        <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
+
+
       </head>
       <body>
         {props.children}
@@ -27,19 +31,35 @@ const HTML: FC = (props) => {
   )
 }
 
-const Block: FC = (props: { title: string, description: string }) => {
+const Block: FC = (props: { title: string, description: string, id: number }) => {
   return (
     <div>
-      <h2> Todo Title </h2>
-      <p> Todo Content </p>
-      <button> Delete </button>
-      <button> Edit </button>
+      <h2> {props.title} </h2>
+      <p> {props.description} </p>
+      <button hx-get={`/edit/${props.id}`} hx-target="closest div" hx-swap="outerHTML"> Edit </button>
+      <button hx-delete={`/edit/${props.id}`} hx-target="closest div" hx-swap="outerHTML"> Delete </button>
     </div>
   )
 }
 
+const EditableBlock: FC = (props: { title: string, description: string, id: number }) => {
+  return (
+    <form hx-post={`/edit/${props.id}`}>
+      <label for="title"> Title </label>
+      <input name="title" type="text" id="title" value={props.title} />
+      <br />
+      <label for="description"> Description </label>
+      <textarea name="description" id="description">
+        {props.description}
+      </textarea>
+      <button type='submit'> Save </button>
+      <button hx-delete={`/edit/${props.id}`} hx-target="closest div" hx-swap="outerHTML"> Delete </button>
+    </form>
+  )
+}
+
 const Adder: FC = () => (
-  <form method="post" action="/save">
+  <form hx-post="/save" hx-target="#todo-target" hx-swap="afterend">
     <label for="title"> Title </label>
     <input type="text" name="title" id="title" />
     <br />
@@ -63,13 +83,13 @@ app.post('/save',
     const { title, description } = c.req.valid('form')
 
     try {
-      await db
+      const res = await db
         .insert(todo)
         .values({
           title: title,
           description: description
-        })
-      return c.text('Created.', 201)
+        }).returning()
+      return c.html(<Block title={res[0].title} description={res[0].description} id={res[0].id}></Block>)
     } catch {
       return c.text('Failed to Create', 400)
     }
@@ -79,10 +99,7 @@ app.post('/save',
 app.get('/todos',
   async (c) => {
     const result = await db
-      .select({
-        title: todo.title,
-        description: todo.description
-      })
+      .select()
       .from(todo)
 
     return c.html(
@@ -91,17 +108,63 @@ app.get('/todos',
           result.map(v => <Block
             title={v.title}
             description={v.description}
+            id={v.id}
           />)
         }
       </Fragment>
     )
-  })
+  }
+)
 
 app.get('/', (c) => c.html(
   <HTML>
-    <h1> HTML with proops </h1>
+    <h1 hx-get="/todos" hx-target="#todo-target" hx-trigger="load"> HTML with proops </h1>
     <Adder />
+    <div id="todo-target"></div>
   </HTML>
 ))
+
+app.get('/edit/:id', async (c) => {
+  try {
+    const id = parseInt(c.req.param('id'))
+    const res = await db.select().from(todo).where(eq(todo.id, id)).limit(1)
+    return c.html(
+      <EditableBlock title={res[0].title} description={res[0].description} id={id} />
+    )
+  } catch (e) {
+    if (e instanceof Error) console.log(e.message)
+    return c.text('Not Found.', 500)
+  }
+})
+
+app.post('/edit/:id',
+  validator('form', (value, c) => {
+    const title = value['title']
+    const description = value['description']
+    if (typeof title === 'string' && typeof description === 'string') {
+      return { title: title, description: description }
+    }
+    return c.text('Invalid Response', 400)
+  }),
+  async (c) => {
+    try {
+      const id = parseInt(c.req.param('id'))
+      const { title, description } = c.req.valid('form')
+      const res = await db.update(todo).set({ title: title, description: description }).where(eq(todo.id, id)).returning()
+      return c.html(
+        <Block title={res[0].title} description={res[0].description} id={id} />
+      )
+    } catch (e) {
+      if (e instanceof Error) console.log(e.message)
+      return c.text('Not Found.', 500)
+    }
+  })
+
+app.delete('/edit/:id', async (c) => {
+    const id = parseInt(c.req.param('id'))
+    await db.delete(todo).where(eq(todo.id, id))
+    return c.html("")
+  }
+)
 
 serve(app)
